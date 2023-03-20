f62b94036133db25deaac053269c69dc2b54292e
iamrahulrnair <rahul.r@maplelabs.com>
2023-03-20 18:02:33 +0530

    update firebase config, test deployment

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,10 @@
 import * as admin from 'firebase-admin';
-import serviceAccountKey from '../../service_acc_key.json';
 // firebase-admin settings
 const app = !admin.apps.length
   ? admin.initializeApp({
-      credential: admin.credential.cert(serviceAccountKey),
+      credential: admin.credential.cert(
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+      ),
     })
   : admin.app();
 export const db = app.firestore();
