ea2fd51af5e1f54ff1c5ddef96e942932f70fdb7
iamrahulrnair <rahul.r@maplelabs.com>
2023-03-20 10:57:18 +0530

    update config

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,15 @@
 import * as admin from 'firebase-admin';
+// firebase-admin settings
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert({
         type: 'service_account',
         project_id: 'maplelabs-2',
         private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-        private_key: JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY),
+        private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(
+          /\\n/gm,
+          '\n'
+        ),
         client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
         auth_uri: 'https://accounts.google.com/o/oauth2/auth',
