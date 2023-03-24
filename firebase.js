b59c0cfe53372f78445e083fba77f3f2d8b76e2d
iamrahulrnair <rahul.r@maplelabs.com>
2023-03-24 20:10:17 +0530

    add client firestore db

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'maplelabs-2.firebaseapp.com',
+  projectId: 'maplelabs-2',
+  storageBucket: 'maplelabs-2.appspot.com',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+export const dbClient = app.firestore();
